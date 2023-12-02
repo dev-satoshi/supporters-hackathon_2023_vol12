d77f597ce7c5b2a43626de02544cbf98fc673339
takuyakubo <takuyakubo7140@gmail.com>
2023-12-03 03:20:50 +0900

    add: フォーム入力内容をDBに対して保存する処理を追加しました

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
+# Create your models here.
 class Post(models.Model):
     location_name = models.CharField(max_length=100)
-    address = models.TextField()
+    address = models.CharField(max_length=200)
+    OPTION_ONE = '1'
+    OPTION_TWO = '2'
+    OPTION_THREE = '3'
+    CATEGORY = [
+        (OPTION_ONE, 'カフェ'),
+        (OPTION_TWO, 'レストラン'),
+        (OPTION_THREE, '公園')
+    ]
+    category = models.CharField(max_length=2, choices=CATEGORY, default=OPTION_ONE)
     post_content = models.TextField()
 
     def __str__(self):
-        return self.location_name
+        return self.post_content
--- pages/views.py
+++ pages/views.py
@@ -19,3 +19,15 @@ class CreatePostView(FormView):
     template_name = "posts/post_create.html"
     form_class = CreatePostForm
     success_url = "/success/"  # フォーム送信成功後にリダイレクトするURL
+
+    def form_valid(self, form):
+        #  フォームのデータ保存などの処理
+        form.save()
+        print("ok")
+        # form.cleaned_dataを使用してフォームのデータにアクセス
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        #  フォームのデータが無効な場合の処理
+        print(form.errors)
+        return super().form_invalid(form)
