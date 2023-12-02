7665843ace34329d5202ab6db000ac17a74bab3d
takuyakubo <takuyakubo7140@gmail.com>
2023-12-02 18:46:46 +0900

    pages/serializers.py

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Post
+
+class PostModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post()  # シリアライズするモデル
+        fields = ["title", "address", "category", "content"]  #シリアライズするフィールド
