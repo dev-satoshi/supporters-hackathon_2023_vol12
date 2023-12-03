03d643e57d21e6e0ed89581b97a8fe07af39d1e7
dev-satoshi <dev.satoshi.1016@gmail.com>
2023-12-03 14:43:27 +0900

    fix: 投稿できなかったのを修正

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 from .views import HomePageView, PostCreateView, PostDeleteView, PostDetailView
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("post/<int:pk>/", PostDeleteView.as_view(), name="delete-post"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
